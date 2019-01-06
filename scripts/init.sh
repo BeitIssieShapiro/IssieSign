32b2197417f66933bae7eecad299216741c523c2
Ariel Bentolila <ariel.bentolila@sap.com>
2019-01-06 10:27:36 +0200

    git ignore

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,7 @@
 MEDIA_PATH=/Users/i022021/dev/sign_lang/IssieSignMedia
 
-cp -R $MEDIA_PATH/videos ../public/
-cp -R $MEDIA_PATH/images ../src/
+pushd ..
+npm install
+cp -R $MEDIA_PATH/videos public/
+cp -R $MEDIA_PATH/images src/
+popd
